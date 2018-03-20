24596b932ea9b1f38dd150689731c9e1736eeb3c
Anna Lewis <anna.noel.lewis@gmail.com>
2018-03-19 20:21:38 -0600

    Config database for heroku

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,12 @@
 # set the database based on the current environment
-database_name = "bike-share-#{BikeShareApp.environment}"
-db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
+
+if ENV['DATABASE_URL']
+  db = URI.parse(ENV['DATABASE_URL'])
+  database_name = db.path.split('/')[1]
+else
+  database_name = "bike-share-#{BikeShareApp.environment}"
+  db = URI.parse("postgres://localhost/#{database_name}")
+end
 
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
@@ -9,6 +15,6 @@ ActiveRecord::Base.establish_connection(
   :port     => db.port,
   :username => db.user,
   :password => db.password,
-  :database => "#{database_name}",
+  :database => database_name,
   :encoding => 'utf8'
 )
